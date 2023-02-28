572a3c246c15f592232435b20438af3c7f3defb4
Mike Eckel <mikeeckel@hotmail.com>
2023-02-28 04:51:10 -0600

    ConfigurationExtensions class added

---
+++ PuppySharpPdf.Core/PuppySharpPdf.Core/Renderers/Configurations/PuppySharpConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PuppySharpPdf.Core.Renderers.Configurations;
+public static class PuppySharpConfiguration
+{
+    public static IServiceCollection AddPuppySharpPdfCore(this IServiceCollection services)
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        services.AddHttpContextAccessor();
+
+        return services;
+    }
+
+    public static void UsePuppySharpPdfCore(this IApplicationBuilder app)
+    {
+        app.Use((context, next) =>
+        {
+            context.Items["HttpContext"] = context;
+            return next();
+        });
+    }
+
+    public static HttpContext GetHttpContext(this HttpContext context)
+    {
+        return (HttpContext)context.Items["HttpContext"];
+    }
+
+}
